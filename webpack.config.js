7ccdc31f593525d2c564c46512bc908606ac364c
liulka_s <asl.warmhaus@gmail.com>
2023-05-16 20:02:16 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
      output: {
         path: path.resolve(__dirname, './dist'),
          filename: 'bundle.js',
-    // publicPath: 'goit-js-hw-10',
+    publicPath: '/',
         // clean: true,
     },
      plugins: [
