e428dc85885879224ba402925ed4d3beaf861543
liulka_s <asl.warmhaus@gmail.com>
2023-05-16 19:23:05 +0200

    1

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.resolve(__dirname, './dist'),
+    },
+    open: true,
+    compress: true,
+    hot: true,
+        port: 8080,
+     client: {
+      logging: 'none',
+    },
+  },
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+     output: {
+        path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+        // clean: true,
+    },
+     plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      chunks: ['main'],
+    }),
+         new CleanWebpackPlugin(),
+     new webpack.HotModuleReplacementPlugin(),
+    ],
+     module: {
+        rules: [
+            // JavaScript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+             },
+            // изображения
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+             },
+            // шрифты и SVG
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+             },
+            // CSS, PostCSS
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+        ],
+    }
+}
