b263fa278d16e2213a67f209e462bd3bf2c9efdb
liulka_s <asl.warmhaus@gmail.com>
2023-05-17 07:56:11 +0200

    1

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     },
      output: {
         path: path.resolve(__dirname, './dist'),
-         filename: 'bundle.js',
+         filename: 'index.js',
     publicPath: '/',
         // clean: true,
     },
