56ff578228f0a6cd2394dafac694d27fe9c47a9f
liulka_s <asl.warmhaus@gmail.com>
2023-05-17 09:45:45 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development',
-    devServer: {
+  mode: 'development',
+  devServer: {
     historyApiFallback: true,
     static: {
       directory: path.resolve(__dirname, './dist'),
@@ -13,54 +13,57 @@ module.exports = {
     open: true,
     compress: true,
     hot: true,
-        port: 8080,
-     client: {
+    port: 8080,
+    client: {
       logging: 'none',
     },
   },
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
-    },
-     output: {
-        path: path.resolve(__dirname, './dist'),
-         filename: 'index.js',
-         publicPath: '/goit-js-hw-10/',
-    library: {
-    type: 'module',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
   },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index.js',
+    publicPath: '/goit-js-hw-10/',
+    library: {
+      type: 'module',
     },
-     plugins: [
+  },
+  experiments: {
+    outputModule: true,
+  },
+  plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
       chunks: ['main'],
     }),
-         new CleanWebpackPlugin(),
-     new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      // изображения
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      // шрифты и SVG
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      // CSS, PostCSS
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
     ],
-     module: {
-        rules: [
-            // JavaScript
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-             },
-            // изображения
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                type: 'asset/resource',
-             },
-            // шрифты и SVG
-            {
-                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-                type: 'asset/inline',
-             },
-            // CSS, PostCSS
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
-            },
-        ],
-    }
-}
+  },
+};
