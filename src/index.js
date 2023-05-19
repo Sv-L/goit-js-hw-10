c412702646b5c66d39a34d5ab2902d13d1df90e9
liulka_s <asl.warmhaus@gmail.com>
2023-05-19 17:10:59 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import {fetchCountries} from './fetchCountries';
+import { fetchCountries } from './fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import _ from 'lodash';
 
@@ -12,41 +12,42 @@ const searchBoxEl = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
 
-searchBoxEl.addEventListener('input', _.debounce(onInputSearch, DEBOUNCE_DELAY));
+searchBoxEl.addEventListener(
+  'input',
+  _.debounce(onInputSearch, DEBOUNCE_DELAY)
+);
 
 function onInputSearch(e) {
   const search = e.target.value.trim();
-  
+
   if (search === '') {
-    clearMarkup(); 
-    return
+    clearMarkup();
+    return;
   }
-  
-  fetchCountries(search)
-  .then(renderCountryList)
-  .catch(handleError);
+
+  fetchCountries(search).then(renderCountryList).catch(handleError);
 }
 
 function renderCountryList(countries) {
   const numberOfCountries = countries.length;
 
   if (numberOfCountries > 10) {
-    clearMarkup()
+    clearMarkup();
     Notify.info('Too many matches found. Please enter a more specific name.');
-    return
+    return;
   }
   if (numberOfCountries === 1) {
     createOneCountryMarkup(countries);
   } else {
     clearMarkup();
-    createCountryListItemMarkup(countries);  
+    createCountryListItemMarkup(countries);
   }
 }
 
-function clearMarkup () {
+function clearMarkup() {
   countryListEl.innerHTML = '';
   countryInfoEl.innerHTML = '';
-};
+}
 
 function handleError(error) {
   console.log(error);
@@ -60,25 +61,28 @@ function createOneCountryMarkup(countriesArray) {
         <img src ='${country.flags.svg}' width='30' height='20' />
         <h2>${country.name.official}</h2>
       </div>`;
-    
+
   const languagesAsString = Object.values(country.languages).join(', ');
   const markupInfo = `
       <p><b>Capital:</b> ${country.capital}</p>
       <p><b>Population:</b> ${country.population}</p>
       <p><b>Languages:</b> ${languagesAsString}</p>`;
-    
+
   countryListEl.innerHTML = markupTitle;
   countryInfoEl.innerHTML = markupInfo;
 }
 
 function createCountryListItemMarkup(countries) {
   const countriesListMarkup = countries
-      .map(country =>  `<li>
+    .map(
+      country => `<li>
     <div class='wrap'>
       <img src='${country.flags.svg}' width='30' height='20' />
       <p data-code='${country.ccn3}'>${country.name.official}</p>
     </div>
   </li>
-`).join('');
- countryListEl.innerHTML = countriesListMarkup;
-} 
+`
+    )
+    .join('');
+  countryListEl.innerHTML = countriesListMarkup;
+}
